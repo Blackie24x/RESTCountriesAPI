4cf265cfea8547262a0388f1733aed1ea647dfe0
Blackie24x <kacper.kraczkowski24@gmail.com>
2022-08-11 14:12:38 +0200

    fixing routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import Header from "./components/Header";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import CountriesPanel from "./components/CountriesPanel";
 import Country from "./components/Country";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/RESTCountriesAPI"
-          element={
-            <div className="App">
-              <Header></Header>
-              <Routes>
-                <Route path="/" element={<CountriesPanel />} />
-                <Route path="/:id" element={<Country />} />
-              </Routes>
-            </div>
-          }
-        ></Route>
-      </Routes>
-    </BrowserRouter>
+    <HashRouter>
+      <div className="App">
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<CountriesPanel />} />
+          <Route path="/:id" element={<Country />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
